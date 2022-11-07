392ddb051d96148be395aa5b2bfb25578b7b3abf
GaahNoel <ga.noel@outlook.com>
2022-11-07 09:41:55 -0300

    chore: remove axios and change to node 18

--- src/helpers/get-account-ids.js
+++ src/helpers/get-account-ids.js
@@ -1,9 +1,9 @@
-import axios from "axios";
 import { env } from "../config/env.js";
 
 export async function getAccountIds() {
   try {
-    const { data } = await axios.get(`${env.SBDD_API_URL}/api/account/all`)
+    const response = await fetch(`${env.SBDD_API_URL}/api/account/all`)
+    const data = await response.json()
     return data.accountIds;
   } catch(error) {
     console.error(error)
